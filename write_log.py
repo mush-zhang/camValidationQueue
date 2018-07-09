7646d01706cf654beba05f533163bfc27df97841
LING ZHANG <junelena1313@gmail.com>
2018-07-09 14:40:48 -0400

    add write log

--- write_log.py
+++ write_log.py
@@ -1,6 +1,7 @@
 """
 This file will be used for recording log messages when:
 1. There is more than 1 cameras in the db with the same retrieval method.
-   This signals a problem in the db.
-2. 
+   This signals a problem in the db. The camera information shold be recorded.
+2. There is one camera with the same retrieval method when new camera adds,
+   but the information of the new camera is not exactly the same with the older one,
 """
