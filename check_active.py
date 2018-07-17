078e097fd49c0d69b0d4ba4c6f47cd8a6f6e72a0
Jia En Chua <chuaj@purdue.edu>
2018-07-17 10:43:41 -0400

    A lil doc string

--- check_active.py
+++ check_active.py
@@ -2,6 +2,19 @@ from getFramerate import *
 
 
 def check_active(cam):
+    """
+    To check if a cam is active, we determine if the frame rate is more than 0.
+    To get the frame rate, we need 3 files: getFramerate.py, camera.py, stream_parser.py (logic flows in this order)
+    Basically the program use urllib.open function to download a stream of jpeg images and determine the time between
+    first and second image.(which is start and end image)
+
+    :param cam:
+        A camera object
+
+    :return:
+        A boolean to tell if the camera is active
+
+    """
     url = None
     if cam['type'] == 'ip':
         url = cam['video_path']
--- compare_cam.py
+++ compare_cam.py
@@ -3,15 +3,17 @@ import requests
 
 def compare_cam(res):
     """
-    Todo: Compare by pixel wise
+    Todo: Compare in pixel wise
     Retrieval method is confirmed to be the same by check_exist function
     but there are other attributes like lat and long could be different between the old and new cam.
-    If they are different, we return a flag to let the health worker to use the new cam.
+    If they are different, we return a boolean flag to tell the health worker to use the new cam.
 
     :param res:
         New camera object
+
     :return:
         Boolean to tell if the cam's attributes are different
+
     """
     if len(res) == 0:
         return False
